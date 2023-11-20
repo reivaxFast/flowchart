f7d95e2ceaf1937ea70a7e21967167b8fabd3d1d
reivaxFast <xjehoward@gmail.com>
2023-11-20 23:26:58 +0000

    added smooth shading, quality of life and futurepr

--- classes.py
+++ classes.py
@@ -1,9 +1,43 @@
 import pygame
 
 class draggable_box:
-    def __init__(self, x, y) -> None:
-        self.x = x
-        self.y = y
+    def __init__(self, x: int, y: int, w: int, h: int, normal_colour: tuple = (255, 255, 255), shaded_colour: tuple = (200, 200, 200)) -> None:
+        self.x = x #x position
+        self.y = y #y position
+        self.w = w #width
+        self.h = h #height
+        self.colour = (255, 255, 255) #colour
+        self.selected = True #is the box being moved?
+        self.mpessedlast = False #was the mouse being pressed last frame (so the box can only be selected if clicked on)
+        self.normal_colour = normal_colour
+        self.shaded_colour = shaded_colour
     def update(self, screen):
-        self.x, self.y = pygame.mouse.get_pos()
-        pygame.draw.rect(screen, pygame.Color("white"), pygame.Rect((self.x, self.y), (100,100)))
+        mx, my = pygame.mouse.get_pos() # getting mouse position
+        mpressed, _, _ = pygame.mouse.get_pressed() #getting mouse state
+        if self.hover() and not self.selected:
+            c, _, _ = self.colour #getting the colour
+            p = max(c-10, self.shaded_colour[0]) #for a smooth darkening
+            self.colour = (p, p, p)
+            if mpressed and not self.mpessedlast:
+                self.selected = True
+                self.offsetx = self.x - mx
+                self.offsety = self.y - my
+        else:
+            c, _, _ = self.colour
+            p = min(c+10, self.normal_colour[0])
+            self.colour = (p, p, p)
+        
+        if self.selected and mpressed:
+            self.x = mx + self.offsetx
+            self.y = my + self.offsety
+        else:
+            self.selected = False
+        pygame.draw.rect(screen, self.colour, pygame.Rect((self.x, self.y), (self.w,self.h)))
+        self.mpessedlast = mpressed
+    
+    def hover(self):
+        mx, my = pygame.mouse.get_pos()
+        if self.x <= mx <= self.x + self.w and self.y <= my <= self.y + self.h:
+            return True
+        else:
+            return False
--- flow.py
+++ flow.py
@@ -1,17 +1,24 @@
 import pygame, sys, classes
 from pygame.locals import *
-pygame.init()
 
+pygame.init()
+pygame.display.set_caption("Flowchart")
+clock = pygame.time.Clock()
+framerate = 60
 width, height = 1920, 1009
 window = pygame.display.set_mode((width, height), RESIZABLE)
-pygame.display.set_caption("Flowchart")
-box = classes.draggable_box(10, 10)
+box = classes.draggable_box(10, 10, 100, 100)
+
 while True:
+    w, h= pygame.display.get_surface().get_size()
+    pygame.draw.rect(window, (31, 31, 31), pygame.Rect(0, 0, w, h))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
     mx, my = pygame.mouse.get_pos()
     mpressed, _, _ = pygame.mouse.get_pressed()
+    
     box.update(window)
     pygame.display.flip() #update
+    clock.tick(framerate)
